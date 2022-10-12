e2e7dda2cef279043a3f8f8fedec62d5fa3be29c
Dimitri-F <dimitri.fonsat@gmail.com>
2022-10-12 12:49:50 +0200

    mise en place firebase

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 const fields = document.querySelectorAll(".textb input");
 const btn = document.querySelector(".btn");
-const password = document.querySelectorAll(".show-password");
 
+/* fonction qui vérifie si les champs du formulaire ne sont pas vides et change le bouton*/
 function check() {
-    for (var i = 0; i < fields.length; i++) {
+    for (let i = 0; i < fields.length; i++) {
         if (fields[i].value != "") {
             btn.disabled = false;
         } else {
@@ -13,21 +13,26 @@ function check() {
     } 
 }
 
-for (var i = 0; i < fields.length; i++) {
+/* ecouteur évenement sur chaque champs qui déclenche la fonction check */
+for (let i = 0; i < fields.length; i++) {
     fields[i].addEventListener("keyup", check);
 }
 
-
+/* écouteur d'événements pour rendre lisible les password */
 document.querySelector(".show-password").addEventListener("click" , 
 function(){
     if(this.classList[2] == "fa-eye-slash"){
         this.classList.remove("fa-eye-slash");
         this.classList.add("fa-eye");
-        fields[1].type = "text";
+        for (let i = 1; i < fields.length; i++) {
+            fields[i].type = "text";
+        }
     }else {
         this.classList.remove("fa-eye");
         this.classList.add("fa-eye-slash");
-        fields[1].type = "password";
+        for (let i = 1; i < fields.length; i++) {
+            fields[i].type = "password";
+        }
     }
 }
 )
